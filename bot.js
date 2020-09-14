6b3cfa18cda031bb6c287826a5715a2d8ddd0425
Sourabh Anant Kulkarni <67724159+souru98@users.noreply.github.com>
2020-09-14 14:01:21 +0530

    update

---
+++ bot.js
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const {
+    ActivityHandler
+} = require('botbuilder');
+
+class MyBot extends ActivityHandler {
+    // eslint-disable-next-line no-useless-constructor
+    constructor() {
+        super();
+    }
+
+    async getStatus() {
+
+    }
+
+    async onTurn(turnContext) {
+        const text = turnContext.activity.text;
+        // Check API
+        if (/^Check API.*/i.test(text)) {
+            // Retrieve the news
+            const messages = await this.getStatus();
+            for (const message of messages) {
+                turnContext.sendActivity(message);
+            }
+        } else if (/^help$/i.test(text)) {
+            // Help text
+            turnContext.sendActivity('You can ask me for the news with `Check API`');
+        } else {
+            // Do nothing
+        }
+    }
+}
+
+module.exports.MyBot = MyBot;
